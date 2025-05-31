ff9c31cb307a7012fca24dcf882a9012d8de1ccf
Lívia Lyrio <LiviaLyrio08@gmail.com>
2025-05-31 20:27:58 -0300

    adicionei estrutura de conexao com bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Farmacia } from './farmacias/farmacias.model';
+import { FarmaciasModule } from './farmacias/farmacias.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'livialyrio',
+      password: '123456',
+      database: 'db_farmaciasDigitaisSociais',
+      models: [Farmacia],
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    FarmaciasModule,
+  ],
 })
 export class AppModule {}
