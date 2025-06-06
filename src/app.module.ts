82755d78f9c3cded8ea385bde2f5ebc22254bac7
Lívia Lyrio <LiviaLyrio08@gmail.com>
2025-06-06 18:04:52 -0300

    Completando swagger farmacia e doacao remedio + adicionei models app.module e alterei a porta na main

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,13 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Farmacia } from './farmacias/farmacias.model';
-
+import { DoacaoRemedio } from './doacao-remedio/doacao-remedio.model';
+import { DoacaoRemedioModule } from './doacao-remedio/doacao-remedio.module';
 import { FarmaciasModule } from './farmacias/farmacias.module';
+import { Usuario } from './usuarios/usuarios.model';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { Remedio } from './remedios/remedios.model';
+import { RemediosModule } from './remedios/remedios.module';
 
 @Module({
   imports: [
@@ -16,11 +21,11 @@ import { FarmaciasModule } from './farmacias/farmacias.module';
       username: 'livialyrio',
       password: '123456',
       database: 'db_farmaciasDigitaisSociais',
-      models: [Farmacia],
+      models: [Farmacia, DoacaoRemedio, Usuario, Remedio],
       autoLoadModels: true,
       synchronize: true,
     }),
-    FarmaciasModule,
+    FarmaciasModule, DoacaoRemedioModule, UsuariosModule, RemediosModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/farmacias/dto/create-farmacia.dto.ts
+++ src/farmacias/dto/create-farmacia.dto.ts
@@ -1,22 +1,27 @@
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateFarmaciaDto {
+  @ApiProperty({ example: '22222-222', description: 'CEP da farmácia' })
   @IsString()
   @IsNotEmpty()
   cep: string;
 
-  @IsString()
+  @ApiProperty({ example: 'Rio de Janeiro', description: 'cidade da farmácia' })
   @IsNotEmpty()
   cidade: string;
 
+  @ApiProperty({ example: 'Botafogo', description: 'Bairro da farmácia' })
   @IsString()
   @IsNotEmpty()
   bairro: string;
 
+  @ApiProperty({ example: 'Rua das Flores', description: 'Logradouro da farmácia' })
   @IsString()
   @IsNotEmpty()
   logradouro: string;
 
+  @ApiProperty({ example: '123', description: 'Número da farmácia' })
   @IsString()
   @IsNotEmpty()
   numero: number;
