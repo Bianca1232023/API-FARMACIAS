c5e1e24f88c768034a18755b16c04c985b6c4a49
Bianca1232023 <150062646+Bianca1232023@users.noreply.github.com>
2025-07-11 02:47:37 -0300

    frontend adicionado a main

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Banner from "@/components/navbar/banner/Banner";
+import Navbar from "@/components/navbar/Navbar";
+
+
+
+export default function HomePage() {
+  return (
+    <main>
+      <Navbar />
+      <Banner />
+    </main>
+  );
+}
+
